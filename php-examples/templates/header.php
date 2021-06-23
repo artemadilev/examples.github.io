7e4112875fc4b0850aa67de490b93e1c239296d5
Artem Adilev <artemadilev@ya.ru>
2021-06-24 02:04:47 +0500

    Create header.php

---
+++ php-examples/templates/header.php
@@ -0,0 +1,24 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include $_SERVER['DOCUMENT_ROOT'] . '/include/functions.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/include/config.php';
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/styles.css" type="text/css">
+    <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+    <title>Gallery</title>
+</head>
+<body>
+<div class="content">
